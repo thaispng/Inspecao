c797bb5b7ea8944b0b1848a44c0ee7e74ecccf4b
thais ventura <thaisvtr26@edu.unifor.br>
2024-05-29 14:28:29 -0300

    styles: favicon.ico

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,11 +6,16 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Inspeção",
+  description: "Sistema de inspeção de obras",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
+      <head>
+        <link rel='icon' href="/favicon.ico" />
+        <title>{metadata.title}</title>
+      </head>
       <body className={inter.className}>
         <AuthProvider> {children}</AuthProvider>
        </body>
